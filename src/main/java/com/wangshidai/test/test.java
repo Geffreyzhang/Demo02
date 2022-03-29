6707e4eff4c2c3eb802e94b561f0ead4b202345e
tom_pro <goodMorning_pro@atguigu.com>
2022-03-29 22:23:05 +0800

    第一次创建本地库并提交

---
+++ src/main/java/com/wangshidai/test/test.java
@@ -0,0 +1,8 @@
+package com.wangshidai.test;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("这是第一次的创建的类");
+        System.out.println("这是第二次创建的类");
+    }
+}
